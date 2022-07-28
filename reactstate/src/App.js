b61ca51e096bb22710b6fc0cfe42d3c546676a5a
Your Name <hlalirefka@gmail.com>
2022-07-28 13:30:48 +0200

    first commit

--- reactstate/src/App.js
+++ reactstate/src/App.js
@@ -1,23 +1,54 @@
 import React, { Component } from "react";
+import Person from "./componenet/Person";
+import "./App.css";
 
 class App extends Component {
   state = {
-    fullName: "refka hlali",
-    bio: "web develloper",
-    imgSrc: "",
-    profession: "this is my profile",
-    showState: "flase",
+    Person: {
+      fullName: "refka hlali",
+      bio: "web develloper",
+      imgSrc: "",
+      profession: "this is my profile",
+    },
+    showState: false,
   };
   toggleshowState = () => {
     this.setState({ showState: !this.state.showState });
   };
+
   render() {
     return (
       <div>
-        <h1> My profile</h1>
-        <button className="visibility-btn" onClick={this.toggleVisibility}>
-          {this.state.showState ? "Hide person" : "Show person"}
+        <h1
+          style={{
+            color: "black",
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          {" "}
+          My profile
+        </h1>
+
+        <button
+          style={{
+            display: "block",
+            margin: "auto",
+            color: "black",
+          }}
+          className="visibility-btn"
+          onClick={this.toggleshowState}
+        >
+          {this.state.showState ? "Hide Person" : "Show Person"}
         </button>
+        {this.state.showState && (
+          <Person
+            fullName={this.state.Person.fullName}
+            bio={this.state.Person.bio}
+            profession={this.state.Person.profession}
+          />
+        )}
       </div>
     );
   }
